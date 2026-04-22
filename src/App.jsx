ae5281fae46989893ba07bdc82c27ba3fbc2d600
Mahdi Hasan <mahdihasanwebsite@gmail.com>
2026-04-22 20:38:17 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {  Routes, Route } from "react-router";
+import Home from './Pages/Home';
+
+const App = () => {
+  return (
+  
+     <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+    
+  )
+}
+
+export default App
