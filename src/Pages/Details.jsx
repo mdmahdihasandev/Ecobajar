a51e69b54eda56e618043497a52c8eba37588fd4
Mahdi Hasan <mahdihasanwebsite@gmail.com>
2026-08-03 18:56:28 +0600

    details-slider-done

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,142 +1,226 @@
-import React, {  useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Container from '../Components/Layuots/Container';
- 
+import React, { useState } from "react";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
+import Container from "../Components/Layuots/Container";
 
-
-
-// import required modules
-import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
-
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
 
 export default function Details() {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
-  console.log(thumbsSwiper);
-  
-  
 
   return (
-    <>
+    <Container>
+      <div className="flex gap-[10px]">
 
-  
-     <Container>
-        <div className='flex gap-[10px] relative'>
+        {/* ================= LEFT SWIPER ================= */}
 
-      <div className='w-[400px] h-[550px] mt-[10px] bg-[red] absolute'>
+        <div className="w-[90px] h-[550px] mt-[10px]">
 
-      <Swiper
-        onSwiper={setThumbsSwiper}
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            direction="vertical"
+            loop={true}
+            spaceBetween={10}
+            slidesPerView={4}
+            navigation={true}
+            freeMode={true}
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper h-[550px]"
+          >
 
-          direction={'vertical'}
-           
-        loop={true}
-        spaceBetween={10}
-        slidesPerView={4}
-        navigation={true}
-        // freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper"
-      >
-       
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-1.jpg"
+                alt=""
+              />
+            </SwiperSlide>
 
-    
-        <SwiperSlide >
-          <img  src="https://swiperjs.com/demos/images/abstract-1.jpg" />
-        </SwiperSlide>
-          <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-9.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-10.jpg" />
-        </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-2.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-3.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-4.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-5.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-6.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-7.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-8.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-9.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-[125px] object-cover cursor-pointer"
+                src="https://swiperjs.com/demos/images/abstract-10.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+          </Swiper>
+
+        </div>
 
 
-      </Swiper>
+        {/* ================= RIGHT BIG SWIPER ================= */}
 
-    gfhfgjhfgjh
+        <div className="w-[550px] h-[550px]">
+
+          <Swiper
+            loop={true}
+            spaceBetween={10}
+            thumbs={{
+              swiper:
+                thumbsSwiper && !thumbsSwiper.destroyed
+                  ? thumbsSwiper
+                  : null,
+            }}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper2 w-full h-full"
+          >
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-1.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-2.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-3.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-4.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-5.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-6.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-7.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-8.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-9.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                className="w-full h-full object-cover"
+                src="https://swiperjs.com/demos/images/abstract-10.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+
+          </Swiper>
+
+        </div>
+
       </div>
-
-
-      {/* secend  */}
-
-       <div className='w-full'>
-       <Swiper
-      
-        loop={true}
-        spaceBetween={10}
-        // navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-9.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/abstract-10.jpg" />
-        </SwiperSlide>
-      </Swiper>
-      
-      </div>
-       </div>
-
-     </Container>
-
-     
-    </>
+    </Container>
   );
 }
